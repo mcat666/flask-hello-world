bef56dae236251313f3597aaa3f45e6eb7272fa3
mcat666 <104911389+mcat666@users.noreply.github.com>
2023-04-10 11:01:53 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,13 +4,13 @@ app = Flask(__name__)
     
 @app.route('/')
 def home():
-    count = 0
+#     count = 0
 #     while True:
 #         print 
 #         count+=1
 #         time.sleep(1)
-        count+=1
-    return ('Hello, World! ',count)
+#         count+=1
+    return ('Hello, World! ')
 
 @app.route('/about')
 def about():
